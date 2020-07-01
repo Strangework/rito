5f95bd11e88135abf85b0993e49f8febd56f46d3
Nicholas Yang Chee <nicholas.yang.chee@gmail.com>
2020-07-01 01:29:46 -0400

    Accepts a summoner name as main arg. No longer crashes when rate is limited

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pdb
 import json
 import requests
-import traceback
 import sys
+import time
+import traceback
 
 CHAMPION_ID_FILENAME = 'champion_id.json'
 
@@ -26,10 +27,18 @@ class Champion:
 
 class Match:
   def __init__(self, match_id):
-    resp = requests.get(
-        MATCH_API.format(match_id, API_KEY))
-    # !! Handle bad status codes wherever APIs are called
-    self._data = json.loads(resp.content)
+    while True:
+      resp = requests.get(
+          MATCH_API.format(match_id, API_KEY))
+      self._data = json.loads(resp.content)
+ 
+      # If rate limited, wait and try again
+      # TODO : Move this to new ConnectionManager class
+      if 'status' not in self._data:
+        break
+      print('Rate limit exceeded - waiting for 5s')
+      time.sleep(5)
+
 
   def get_data(self):
     return self._data
@@ -51,44 +60,48 @@ class Summoner:
     return self._summoner_id
 
 def main():
-  vyle = Summoner("Vylemaw")
+  some_summ = Summoner(sys.argv[1])
   resp = requests.get(
-      MATCH_BY_ACCOUNT_API.format(vyle.get_account_id(), API_KEY))
+      MATCH_BY_ACCOUNT_API.format(some_summ.get_account_id(), API_KEY))
   resp = json.loads(resp.content)
   matches = resp['matches']
 
   for match in matches:
-    match = Match(match['gameId'])
-    match_raw = match.get_data()
-
     try:
+      match = Match(match['gameId'])
+      match_raw = match.get_data()
+
       # Only matchmade Rift, bois
       if match_raw['gameMode'] != 'CLASSIC' or match_raw['gameType'] != 'MATCHED_GAME':
         continue
+
+      # Find desired summoner among participants
+      desired_id = 0
+      for part_id in match_raw['participantIdentities']:
+        if part_id['player']['accountId'] == some_summ.get_account_id():
+          desired_id = part_id['participantId']
+          break
+
+      # Display champion and match stats
+      for part in match_raw['participants']:
+        if part['participantId'] == desired_id:
+          champ_name = Champion.get_name(part['championId'])
+          cs = (part['stats']['totalMinionsKilled'] +
+              part['stats']['neutralMinionsKilled'])
+          game_dur = match_raw['gameDuration']
+          cs_per_min = cs/float(game_dur/60)
+          print("{} : {}cs / {}min {}s = {:0.2f}cs/min".format(
+              champ_name,
+              cs,
+              game_dur//60,
+              game_dur%60,
+              cs_per_min))
+          break
     except:
       extype, value, tb = sys.exc_info()
       traceback.print_exc()
       pdb.post_mortem(tb)
 
-    desired_id = 0
-    for part_id in match_raw['participantIdentities']:
-      if part_id['player']['accountId'] == vyle.get_account_id():
-        desired_id = part_id['participantId']
-        break
-    for summ in match_raw['participants']:
-      if summ['participantId'] == desired_id:
-        champ_name = Champion.get_name(summ['championId'])
-        cs = (summ['stats']['totalMinionsKilled'] +
-            summ['stats']['neutralMinionsKilled'])
-        game_dur = match_raw['gameDuration']
-        cs_per_min = cs/float(game_dur/60)
-        print("{} : {}cs / {}min {}s = {:0.2f}cs/min".format(
-            champ_name,
-            cs,
-            game_dur//60,
-            game_dur%60,
-            cs_per_min))
-        break
 
 if __name__ == '__main__':
   main()
