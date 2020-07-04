897a9556761a0e4ee0f065dded639169f9b4644a
Nicholas Yang Chee <nicholas.yang.chee@gmail.com>
2020-07-03 21:50:29 -0400

    Implemented simple summoner name lookup on webpage

--- webapp.py
+++ webapp.py
@@ -5,10 +5,15 @@ from riot_api import RiotApi, Champion, Match, Summoner
 import cs_per_min
 
 
-@app.route('/<summoner_name>')
-def home(summoner_name):
+@app.route('/')
+def home():
+  return flask.render_template('hello.html')
+
+@app.route('/summoner/<summoner_name>')
+def get_summoner_report(summoner_name):
   full_report = []
   if summoner_name != '':
     some_summ = Summoner(summoner_name)
     report = cs_per_min.generate_report(some_summ, 5)
-  return flask.render_template('hello.html', report=report)
+    payload = {'report': report} 
+  return flask.jsonify(payload)
